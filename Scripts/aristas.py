5d1cfd32b1ad76f9e13642dd586dac542b6b6306
Guillermo-Ribero <guillermo.ribero@urosario.edu.co>
2021-11-11 09:06:06 -0500

    again casi

--- Scripts/aristas.py
+++ Scripts/aristas.py
@@ -12,9 +12,24 @@ def distancia_r2(p1,p2): #p1 y p2 son nodos que tiene componente en x y en y
     #el 100000 lo saque a ojo comparando una distancia en maps y con la que encontre la diferencia fue como 10mts entonces todo good
 
 def pond_distance(D,A,alfa): #DOnde D es la distancia y A es el num de acidentes entre los puntos A y B
-    return (1-alfa)*D + alfa*A
+    return (1-alfa)*D + alfa*A #Aun no se como quitar las sobrantes pues en cuanto a distancia no uenta y accidentes pues tampoco ya que si son 0 peta.
+    #Se me ocurre que si lo veo en
 #
-def num_accidentes(p1,p2):
+def acc(p1,p2, dir_acci: list):
+    accidentes = []
+    for i in dir_acci:
+        if i[0] == j[0] or i[1] == j[1]: #Es decir comparten direccion o calle o lo que sea que se llame en otras palabras son conexos
+            num_acc_j += 1 #Por cada uno
+
+def num_accidentes(name1 = 'adyacencia.csv' , name2='solo_dir_accidentes.csv'): #Falta crear el de dire de accidentes
+    #Asumiendo que tengo el punto x,y del accidente en coordenadas
+    #Para todo nodo conexo hay al menos un accidente y este lo inventamos, la idea es que los que comparten calle o carrera tienen un accidente esto es para que los accidentes 0 que son atraves de un edificio no joda todo
+    df1 = pd.read_csv(name1)
+    dir_coord = df1.values.tolist() # Lista de listas
+    #coord = coord[1:] #Mocho la dupla de nombres Esto es por si tiene titulo
+    df2 = pd.read_csv(name2)
+    dir_acci = df2.values.tolist() # Lista de listas
+
 
 
 
@@ -25,5 +40,6 @@ def filter_aristas(name = 'C:\Users\USUARIO\Documents\Universidad\UR\Cuarto_Seme
     coord = coord[1:] #Mocho la dupla de nombres
     #Faltaria eliminar las aristas que tienen mucha distancia o muy poca
     graph = [ dis(i,j) for i in coord for j in coordenadas if i!=j and dis(i,j) not in graph and dis(j,i) not in graph]
+    #Puedo ahorrarme esta linea de abajo si pongo de una que la distancia sea menor o igual a 300
     short_distances_graph = [(m[0],m[1],m[2]) for m in graph if m[2]<=300] #Reduzco distancias a solo 300mts como max
     pond_distance_graph = [(h[0],h[1],pond_distance(h[2],num_accidentes(h[0],h[1]),0.05)) for h in short_distances_graph] #Flta poner el alfa como input del usuario
